b4895b1209ee8bbd126cc42cb747ce171cc28a4c
Alexandre Gerona <alecgerona@gmail.com>
2020-07-24 00:08:05 +0800

    feat(project-card): add dynamic svg icons for frameworks

---
+++ src/components/icons/framework-icon.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import Android from "./android";
+import Angular from "./angular";
+import Django from "./django";
+import Flask from "./flask";
+import Gatsby from "./gatsby";
+import Python from "./python";
+
+interface FrameworkIconProps {
+  icon: string;
+}
+
+const FrameworkIcon: React.FC<FrameworkIconProps> = ({ icon }) => {
+  const icons: { [Key: string]: React.FC } = {
+    android: Android,
+    angular: Angular,
+    django: Django,
+    flask: Flask,
+    gatsby: Gatsby,
+    python: Python,
+  };
+  const Icon = icons[icon];
+  return <Icon />;
+};
+export default FrameworkIcon;
