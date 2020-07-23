b2da7366a11cddb76c280b9b1422b37ea0ec36e7
Alexandre Gerona <alecgerona@gmail.com>
2020-07-24 03:47:25 +0800

    fix(blog-card): make the whole blog-card clickable

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -17,18 +17,18 @@ const BlogCard: React.FC<ProjectCardProps> = ({
   readingTime,
 }) => {
   return (
-    <div className="text-white light:text-black m-6 max-w-2xl project-card mt-10">
-      <Link to={slug}>
+    <Link to={slug}>
+      <div className="text-white light:text-black m-6 max-w-2xl project-card mt-10">
         <p className="flex text-xl font-sans text-primary-light">{title}</p>
-      </Link>
-      <p className="text-left mt-6 text-sm leading-7 tracking-wide">
-        {excerpt}
-      </p>
-      <div className="text-sm mt-6 flex tracking-wide leading-7 font-bold">
-        <span>{date}</span>
-        <span className="ml-6 text-orange uppercase">{readingTime}</span>
+        <p className="text-left mt-6 text-sm leading-7 tracking-wide">
+          {excerpt}
+        </p>
+        <div className="text-sm mt-6 flex tracking-wide leading-7 font-bold">
+          <span>{date}</span>
+          <span className="ml-6 text-orange uppercase">{readingTime}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 export default BlogCard;
