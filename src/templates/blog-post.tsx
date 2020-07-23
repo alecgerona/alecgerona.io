df48f08003998400d61c3aac965d4cc7e9e5c695
Alexandre Gerona <alecgerona@gmail.com>
2020-07-24 00:37:58 +0800

    refactor: remove unneeded comments

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -3,7 +3,6 @@ import { Link, graphql } from "gatsby";
 import Img from "gatsby-image";
 
 import SEO from "../components/seo";
-// @ts-ignore
 import BlogLayout from "../components/blog-layout";
 
 interface BlogPostTemplateProps {
