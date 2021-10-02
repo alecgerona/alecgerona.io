2b724beab2eb789a3464ee86e3fbb6661024cfb5
Alexandre Gerona <alecgerona@gmail.com>
2021-10-02 11:10:55 +0800

    fix(rss): add default config for gatsby-plugin-feed to work

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,59 @@ module.exports = {
     },
     `gatsby-plugin-typescript`,
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: `/rss.xml`,
+            title: `alecgerona.io RSS Feed`,
+          },
+        ],
+      },
+    },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-google-analytics`,
@@ -96,20 +148,20 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-robots-txt",
+      resolve: `gatsby-plugin-robots-txt`,
       options: {
         resolveEnv: () => NETLIFY_ENV,
         env: {
           production: {
-            policy: [{ userAgent: "*" }],
+            policy: [{ userAgent: `*` }],
           },
           "branch-deploy": {
-            policy: [{ userAgent: "*", disallow: ["/"] }],
+            policy: [{ userAgent: `*`, disallow: [`/`] }],
             sitemap: null,
             host: null,
           },
           "deploy-preview": {
-            policy: [{ userAgent: "*", disallow: ["/"] }],
+            policy: [{ userAgent: `*`, disallow: [`/`] }],
             sitemap: null,
             host: null,
           },
